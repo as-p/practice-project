2ebfbcf4683fc9d1617e4ad58594fd0a1cada111
Ashok Shinde <shindeashok.as.02@gmail.com>
2020-08-28 17:24:47 +0530

    create panels form employee Detail

---
+++ src/app/employee/listemployee.component.ts
@@ -0,0 +1,52 @@
+import { Employee } from './../models/employee.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listemployee',
+  templateUrl: './listemployee.component.html',
+  styleUrls: ['./listemployee.component.css'],
+})
+export class ListemployeeComponent implements OnInit {
+  employees: Employee[] = [
+    {
+      id: 1,
+      name: 'jon',
+      gender: 'male',
+      email: 'jon@gmail.com',
+      phoneNumber: '9874561238',
+      contactPreference: 'Email',
+      dateOfBirth: new Date('07/24/1998'),
+      department: 'IT',
+      isActive: true,
+      photoPath: 'assets/image/jon.png',
+    },
+    {
+      id: 2,
+      name: 'Sansa',
+      gender: 'female',
+      email: 'sansa@gmail.com',
+      phoneNumber: '8547692415',
+      contactPreference: 'Phone',
+      dateOfBirth: new Date('10/14/2000'),
+      department: 'HR',
+      isActive: true,
+      photoPath: 'assets/image/sansa.png',
+    },
+    {
+      id: 3,
+      name: 'Dany',
+      gender: 'female',
+      email: 'dany@yahoo.com',
+      phoneNumber: '9785641254',
+      contactPreference: 'Email',
+      dateOfBirth: new Date('08/27/1994'),
+      department: 'Computer',
+      isActive: true,
+      photoPath: 'assets/image/dany.png',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
